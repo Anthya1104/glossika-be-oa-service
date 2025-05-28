3c5b1aaeadb3ba2c4cc57afa33fe202055560aea
Anthya1104 <anthya1104@gmail.com>
2025-05-29 03:33:26 +0800

    feat:added cache mechanisms

--- internal/app/handler/recommendation.go
+++ internal/app/handler/recommendation.go
@@ -1,14 +1,18 @@
 package handler
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/Anthya1104/glossika-be-oa-service/internal/app/database"
 	"github.com/Anthya1104/glossika-be-oa-service/internal/app/model"
+	"github.com/Anthya1104/glossika-be-oa-service/internal/app/model/db"
 	"github.com/Anthya1104/glossika-be-oa-service/internal/app/util"
 	"github.com/Anthya1104/glossika-be-oa-service/pkg/errcode"
 	"github.com/Anthya1104/glossika-be-oa-service/pkg/log"
+	"github.com/Anthya1104/glossika-be-oa-service/pkg/redis"
 	"github.com/gin-gonic/gin"
 )
 
@@ -29,27 +33,40 @@ func GetRecommendationListHandler(c *gin.Context) {
 
 	offset := (req.Page - 1) * req.PageSize
 
-	recommendationList, wrapErr := database.GetSqlDb().GetUserRecommendation(c, userID)
-	if wrapErr.RawErr != nil {
-		respondError(c, wrapErr)
-		return
-	}
-	log.C(c).Infof("GetRecommendationListHandler get all recommendationList from DB :%v\n", recommendationList)
+	productList := []db.Product{}
+	// get cache
+	cacheKey := fmt.Sprintf("user_recommendation:%v", userID)
+	cacheVal, err := redis.Client.Get(c, cacheKey).Result()
+	if err == nil && cacheVal != "" {
+		_ = json.Unmarshal([]byte(cacheVal), &productList)
+	} else {
 
-	productIDList := []uint{}
-	for _, v := range recommendationList {
-		productIDList = append(productIDList, v.ProductID)
-	}
+		recommendationList, wrapErr := database.GetSqlDb().GetUserRecommendation(c, userID)
+		if wrapErr.RawErr != nil {
+			respondError(c, wrapErr)
+			return
+		}
+		log.C(c).Debugf("GetRecommendationListHandler get all recommendationList from DB :%v\n", recommendationList)
 
-	productList, wrapErr := database.GetSqlDb().BatchGetProducts(c, productIDList)
-	if wrapErr.RawErr != nil {
-		respondError(c, wrapErr)
-		return
+		productIDList := []uint{}
+		for _, v := range recommendationList {
+			productIDList = append(productIDList, v.ProductID)
+		}
+
+		productList, wrapErr = database.GetSqlDb().BatchGetProducts(c, productIDList)
+		if wrapErr.RawErr != nil {
+			respondError(c, wrapErr)
+			return
+		}
+		log.C(c).Debugf("GetRecommendationListHandler get all productList from DB :%v\n", productList)
+
+		// set cache
+		b, _ := json.Marshal(productList)
+		redis.Client.Set(c, cacheKey, b, time.Minute*10)
 	}
-	log.C(c).Infof("GetRecommendationListHandler get all productList from DB :%v\n", productList)
 
 	pagedrecommendationList, total := util.SliceDataByPaging(productList, offset, req.PageSize)
-	log.C(c).Infof("GetRecommendationListHandler sliced data: %v, total: %v\n", pagedrecommendationList, total)
+	log.C(c).Debugf("GetRecommendationListHandler sliced data: %v, total: %v\n", pagedrecommendationList, total)
 
 	resp := model.GetRecommendationListResp{}
 
